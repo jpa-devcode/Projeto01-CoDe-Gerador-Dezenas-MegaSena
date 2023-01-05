bfe3cdd48fd4aa6174a7090b8f53c486478214eb
Jezimiel Marcondes <jpaintodev@gmail.com>
2023-01-05 17:14:46 -0300

    funcionalidade botão (disparar a geração de dezenas)

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 const min = 1;
 const max = 60;
+const btn = document.querySelector('button');
 var numeroDezenas = 6;
 var dezenas = [];
 
-function gerarDezenas() {
-  var dezena = Math.floor(Math.random() * (max - min) + min);
-  return dezena;
+function main() {
+  dezenas = [];
+  function gerarDezenas() {
+    var dezena = Math.floor(Math.random() * (max - min) + min);
+    return dezena;
+  }
+
+  for (var i = 0; i < numeroDezenas; i++) {
+    dezenas.push(('00' + gerarDezenas()).slice(-2));
+  }
 }
 
-for (var i = 0; i < numeroDezenas; i++) {
-  dezenas.push(('00' + gerarDezenas()).slice(-2));
-}
-
-console.log(dezenas);
+btn.addEventListener('click', function () {
+  main();
+  console.log(dezenas);
+});
