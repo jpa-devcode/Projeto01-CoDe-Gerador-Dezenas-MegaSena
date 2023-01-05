2d988f6b3574b9af171c66905ee5ab1886351bbf
Jezimiel Marcondes <jpaintodev@gmail.com>
2023-01-05 17:24:57 -0300

    Exibição das dezenas na página

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 const min = 1;
 const max = 60;
 const btn = document.querySelector('button');
-var numeroDezenas = 6;
-var dezenas = [];
+const areaDezenas = document.querySelector('.area-dezenas');
+var quantidadeDeDezenas = 6;
+var listaDeDezenas = [];
 
 function main() {
-  dezenas = [];
+  listaDeDezenas = [];
   function gerarDezenas() {
     var dezena = Math.floor(Math.random() * (max - min) + min);
     return dezena;
   }
 
-  for (var i = 0; i < numeroDezenas; i++) {
-    dezenas.push(('00' + gerarDezenas()).slice(-2));
+  for (var i = 0; i < quantidadeDeDezenas; i++) {
+    listaDeDezenas.push(('00' + gerarDezenas()).slice(-2));
   }
 }
 
 btn.addEventListener('click', function () {
   main();
-  console.log(dezenas);
+  areaDezenas.textContent = listaDeDezenas;
+  // console.log(listaDeDezenas);
 });
