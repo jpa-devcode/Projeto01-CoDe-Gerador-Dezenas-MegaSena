e90312a2ea243657f7bb89d129b8b7fe2bb4a447
Jezimiel Marcondes <jpaintodev@gmail.com>
2023-01-06 17:12:26 -0300

    Implementação do botão limpar números exibidos

--- js/main.js
+++ js/main.js
@@ -2,23 +2,37 @@ const min = 1;
 const max = 60;
 const btn = document.querySelector('button');
 const areaDezenas = document.querySelector('.area-dezenas');
+const dezenaUm = document.querySelector('#dezena-01');
+var dezena = 0;
+// var listaSpan = document.getElementById('#area-dezenas');
+// var novoSpan = document.createElement('span');
+
 var quantidadeDeDezenas = 6;
 var listaDeDezenas = [];
 
 function main() {
   listaDeDezenas = [];
   function gerarDezenas() {
-    var dezena = Math.floor(Math.random() * (max - min) + min);
+    dezena = Math.floor(Math.random() * (max - min) + min);
     return dezena;
   }
 
   for (var i = 0; i < quantidadeDeDezenas; i++) {
     listaDeDezenas.push(('00' + gerarDezenas()).slice(-2));
+    var elemento_pai = document.querySelector('.area-dezenas');
+    var titulo = document.createElement('span');
+    var texto = document.createTextNode(listaDeDezenas);
+    titulo.appendChild(texto);
+    elemento_pai.appendChild(titulo);
+    listaDeDezenas = [];
   }
 }
 
 btn.addEventListener('click', function () {
   main();
-  areaDezenas.textContent = listaDeDezenas;
+  // const pegarDezena = listaDeDezenas.map((elemento) => {
+  //   return (areaDezenas.textContent = elemento);
+  // });
+  // criarSpans();
   // console.log(listaDeDezenas);
 });
